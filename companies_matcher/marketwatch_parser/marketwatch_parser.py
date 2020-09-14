5858e80e68fc4da7c795bc75683a9c3d6677df3b
t0theheart <35728736+t0theheart@users.noreply.github.com>
2020-09-14 20:54:00 +0300

    refactored marketwatch_parser

--- companies_matcher/marketwatch_parser/marketwatch_parser.py
+++ companies_matcher/marketwatch_parser/marketwatch_parser.py
@@ -1,44 +1,60 @@
 from bs4 import BeautifulSoup
+from companies_matcher.config import config
 import requests
 
 
+URL = config['marketwatch']['url']
+HEADERS = {'User-Agent': config['service']['userAgent']}
+ENDPOINT = config['marketwatch']['endpoints']['incomeStatement']
+
+
 class MarketwatchParser:
-    def __init__(self, url: str, headers: dict):
-        self._page = None
-        self._url = url
-        self._headers = headers
+    def __init__(self, tickers: list, topics: list):
+        self._url = URL
+        self._headers = HEADERS
+        self._endpoint = ENDPOINT
+        self._tickers = tickers
+        self._topics = topics
+
+    def _request_html(self, ticker: str):
+        url = self._url + f'{ticker}/{self._endpoint}'
+        response = requests.get(url, headers=self._headers)
+        return response.text
 
     @staticmethod
     def _parse_period(soup: BeautifulSoup):
         row = soup.find('tr', class_='topRow')
         return [i.text for i in row.find_all('th', scope='col')][:-1]
 
-    def _request_html(self, ticker: str, endpoint: str):
-        url = self._url + f'{ticker}/{endpoint}'
-        page = requests.get(url, headers=self._headers)
-        self._page = page
+    @staticmethod
+    def _combine_data_with_period(data: dict, period: list):
+        result = {key: {} for key in period}
+        for n, item in enumerate(period):
+            for key in data.keys():
+                values = data[key][n]
+                result[item].update({key: values})
+        return result
 
-    def _parse_html(self, topics: list):
-        result = dict()
-        soup = BeautifulSoup(self._page.text, "html.parser")
+    def _parse_html(self, html: str):
+        data = dict()
+        soup = BeautifulSoup(html, "html.parser")
         rows = soup.find_all('tr')
-
         for row in rows:
             if c := row.find('td', class_='rowTitle'):
                 topic = c.text.strip()
-                if topic in topics:
+                if topic in self._topics:
                     values = [i.text for i in row.find_all('td', class_='valueCell')]
-                    result[topic] = values
-
+                    data[topic] = values
         period = self._parse_period(soup)
+        result = self._combine_data_with_period(data, period)
+        return result, period
 
-        din = {key: {} for key in period}
-        for n, item in enumerate(period):
-            for key in result.keys():
-                values = result[key][n]
-                din[item].update({key: values})
+    def _get_one(self, ticker: str):
+        html = self._request_html(ticker)
+        return {ticker: self._parse_html(html)}
 
-        return din
-
-    def get_data(self, topics: list):
-        return self._parse_html(topics)
+    def get_data(self):
+        result = {}
+        for ticker in self._tickers:
+            result.update(self._get_one(ticker))
+        return result
