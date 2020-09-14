831d05d24b83c48f8468baf0e5f50ca89c59cf8f
t0theheart <35728736+t0theheart@users.noreply.github.com>
2020-09-14 20:00:57 +0300

    refactored finviz parser

--- companies_matcher/finviz_parser/finviz_parser.py
+++ companies_matcher/finviz_parser/finviz_parser.py
@@ -1,34 +1,40 @@
 from bs4 import BeautifulSoup
+from companies_matcher.config import config
 import requests
 
 
+URL = config['finviz']['url']
+HEADERS = {'User-Agent': config['service']['userAgent']}
+
+
 class FinvizParser:
-    def __init__(self, url: str, headers: dict):
-        self._page = None
-        self._url = url
-        self._headers = headers
+    def __init__(self, tickers: list, multiplicators: list):
+        self._url = URL
+        self._headers = HEADERS
+        self._tickers = tickers
+        self._multiplicators = multiplicators
 
     def _request_html(self, ticker: str):
-        page = requests.get(self._url.format(ticker), params={'t': ticker}, headers=self._headers)
-        self._page = page
+        response = requests.get(self._url, params={'t': ticker}, headers=self._headers)
+        return response.text
 
-    def _parse_html(self, multiplicators: list):
+    def _parse_html(self, html: str):
         result = dict()
-        soup = BeautifulSoup(self._page.text, "html.parser")
+        soup = BeautifulSoup(html, "html.parser")
         cells = soup.find_all('td', class_='snapshot-td2-cp')
         for cell in cells:
             key = cell.text
-            if key in multiplicators:
+            if key in self._multiplicators:
                 value = cell.find_next().text
                 result[key] = value
         return result
 
-    def _get_one(self, ticker: str, multiplicators: list):
-        self._request_html(ticker)
-        return {ticker: self._parse_html(multiplicators)}
+    def _get_one(self, ticker: str):
+        html = self._request_html(ticker)
+        return {ticker: self._parse_html(html)}
 
-    def get_data(self, tickers: list, multiplicators: list):
+    def get_data(self):
         result = {}
-        for ticker in tickers:
-            result.update(self._get_one(ticker, multiplicators))
+        for ticker in self._tickers:
+            result.update(self._get_one(ticker))
         return result
