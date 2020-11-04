d6c6eb830cdcd927855d10e4f5006637f8dea118
t0theheart <35728736+t0theheart@users.noreply.github.com>
2020-11-04 12:05:08 +0300

    on button click creating table with companies multiplicators

--- companies_matcher/app/front/static/js/multiplicatorsList.js
+++ companies_matcher/app/front/static/js/multiplicatorsList.js
@@ -11,7 +11,7 @@ function setMultiplicatorsToggles() {
         let toggle = createToggle(item);
         toggles.append(toggle);
     });
-};
+}
 
 function createToggle(name) {
     let toggle = document.createElement('button');
@@ -22,17 +22,60 @@ function createToggle(name) {
     toggle.setAttribute("autocomplete", "off");
     toggle.innerText = name;
     return toggle;
-};
+}
 
-function clickShowTableListener() {
+async function clickShowTableListener() {
     let multiplicators = [];
-    let toggles = document.getElementById('multiplicators-toggles').childNodes;
-
+    let tickers = parseTickers(document.getElementById("input-tickers").value);
+    let toggles = document.getElementById("multiplicators-toggles").childNodes;
     toggles.forEach(function(item){
         if (item.getAttribute("aria-pressed") === "true") {
             multiplicators.push(item.innerText)
         }
     });
-    console.log(multiplicators)
+
+    let body = {
+        "tickers": tickers,
+        "multiplicators": multiplicators
+    };
+
+    let response = await fetch("/matching/multiplicators", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      body: JSON.stringify(body)
+    });
+
+    let result = (await response.json()).result;
+    createTable(result, tickers, multiplicators)
 }
 
+function createTable(data, columnHeaders, rowHeaders) {
+    let placeForTable = document.getElementById("matching-multiplicators");
+
+    let table = document.createElement("table");
+    table.className = "table"
+
+    columnHeaders.unshift("#")
+
+    let headersTr = table.insertRow()
+    columnHeaders.forEach(function(item) {
+        let td = headersTr.insertCell()
+        td.innerHTML = item
+    });
+
+    rowHeaders.forEach(function (rowHeader, i) {
+        let tr = table.insertRow()
+        columnHeaders.forEach(function (columnHeader, j) {
+            let td = tr.insertCell()
+            if (j === 0) {
+                td.innerHTML = rowHeader
+            } else {
+                td.innerHTML = data[columnHeader][rowHeader]
+            }
+        })
+    })
+
+   placeForTable.append(table)
+}
